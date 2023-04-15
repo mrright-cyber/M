904272471bb7d960f9e9d384b873fc2f2cfed537
mrright-cyber <61380864+mrright-cyber@users.noreply.github.com>
2023-04-15 15:12:03 +0800

    log MrRight

--- jdCookie.js
+++ jdCookie.js
@@ -19,7 +19,7 @@ console.log(`\n====================共${CookieJDs.length}个京东账号Cookie==
 console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(
     new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60
     * 60 * 1000).toLocaleString()}=====================\n`)
-
+console.log('>>>>>>>>>>>>>>MrRight 提示：任务正常运行中>>>>>>>>>>>>>>>\n')
 for (let i = 0; i < CookieJDs.length; i++) {
     const index = (i + 1 === 1) ? '' : (i + 1);
     exports['CookieJD' + index] = CookieJDs[i].trim();
